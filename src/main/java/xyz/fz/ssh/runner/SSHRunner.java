5e6343b909a1b6a48e07840031d66193aaead5d5
Fz <natureorz@foxmail.com>
2017-06-18 17:38:40 +0800

    add encrypted mode

--- src/main/java/xyz/fz/ssh/runner/SSHRunner.java
+++ src/main/java/xyz/fz/ssh/runner/SSHRunner.java
@@ -1,11 +1,13 @@
 package xyz.fz.ssh.runner;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import xyz.fz.ssh.cmd.CmdExecutor;
+import xyz.fz.ssh.util.AES128Util;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,6 +19,12 @@ public class SSHRunner implements CommandLineRunner {
     @Value("${sshtaskxml}")
     private String sshtaskxml;
 
+    @Value("${sshtaskencrypted}")
+    private String sshtaskencrypted;
+
+    @Value("${ssh.key}")
+    private String sshKey;
+
     private final CmdExecutor cmdExecutor;
 
     @Autowired
@@ -26,9 +34,16 @@ public class SSHRunner implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        InputStream in = new FileInputStream(new File(sshtaskxml));
-        String taskXml = IOUtils.toString(in);
-        cmdExecutor.exec(taskXml);
+
+        if (strings.length > 0 && StringUtils.equals(strings[0], "pure")) {
+            InputStream in = new FileInputStream(new File(sshtaskxml));
+            String taskXml = IOUtils.toString(in);
+            cmdExecutor.exec(taskXml);
+        } else {
+            InputStream in = new FileInputStream(new File(sshtaskencrypted));
+            String taskEncrypted = IOUtils.toString(in);
+            cmdExecutor.exec(AES128Util.decrypt(sshKey, taskEncrypted));
+        }
         System.out.println("按回车键结束...");
         int r = System.in.read();
     }
